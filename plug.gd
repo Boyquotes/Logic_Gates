971e63499ae0843a5d94f6f4f6eee4ed0b4bd8f1
Gr33nSkull <domen.sedlar123@gmail.com>
2021-04-11 17:33:27 +0200

    button knows when its hoverd upon and when the mouse is pressed

--- plug.gd
+++ plug.gd
@@ -4,14 +4,36 @@ var icon_on = preload("res://plug_on.svg")
 var icon_off = preload("res://plug_off.svg")
 var on = true
 var pos = null
+var selected = false
 
+var hovered = false
+var p = false
 
+func _process(delta):
+	if Input.is_mouse_button_pressed(1) and not p:
+		p = true
+		print("pressed")
+	elif not Input.is_mouse_button_pressed(1):
+		p = false
+
+	  
+		
 func _on_plug_button_down():
 	pos = get_viewport().get_mouse_position()
+	selected = true
 
 
 func _on_plug_button_up():
 	if pos == get_viewport().get_mouse_position():
 		on = false
 		icon = icon_off
+	selected = false
+
+
+func _on_plug_mouse_entered():
+	hovered = true
+
+
+func _on_plug_mouse_exited():
+	hovered = false
 
