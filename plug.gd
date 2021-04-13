7f69a159c2dbdebe66700792e11acdb6d64d8e58
Gr33nSkull <domen.sedlar123@gmail.com>
2021-04-13 19:43:57 +0200

    plug turns on or off depending on the linked node

--- plug.gd
+++ plug.gd
@@ -10,9 +10,19 @@ var selected = false
 var button_area = Rect2(self.rect_position, self.rect_size)
 var lead = null
 
-
+func _process(delta):
+	
+	#verafaying synchronazation with a link node and correcting it
+	if lead != null:
+		if on != lead.on:
+			on = lead.on
+			if on:
+				icon = icon_on
+			else:
+				icon = icon_off
 
 func _input(event):
+	#poveze se z gumbom s katerim ga povezes
 	if event is InputEventMouseButton:
 		if button_area.has_point(self.get_local_mouse_position()):
 			var board = self.get_parent()
@@ -20,7 +30,8 @@ func _input(event):
 				for ii in i.get_children():
 					if "plug" in ii.get_name():
 						if ii.selected:
-							print("we got him")
+							print(ii.on)
+							lead = ii
 
 func _on_plug_button_down():
 	pos = get_viewport().get_mouse_position()
