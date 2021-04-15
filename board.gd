3e0f6da107ed6a588ac60532fb996a5f2bfdf174
Gr33nSkull <domen.sedlar123@gmail.com>
2021-04-15 16:22:00 +0200

    added option to remove output buttons

--- board.gd
+++ board.gd
@@ -1,6 +1,6 @@
 extends ColorRect
 
-var num_outputs = 5 
+var num_outputs = 5
 var output_template = load("res://plug.tscn")
 
 func reposition_buttons():
@@ -21,3 +21,10 @@ func _on_add_output_pressed():
 	reposition_buttons()
 	
 	print(new_output.get_name())
+
+
+func _on_remove_output_pressed():
+		if num_outputs > 6:
+			self.remove_child(self.get_child(num_outputs))
+			num_outputs -= 1
+			reposition_buttons()
