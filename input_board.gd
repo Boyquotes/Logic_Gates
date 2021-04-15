0f12282e5a31c34435ee7cc0537546253a8daa0e
Gr33nSkull <domen.sedlar123@gmail.com>
2021-04-15 15:52:28 +0200

    you can now delete input plugs

--- input_board.gd
+++ input_board.gd
@@ -3,12 +3,7 @@ extends ColorRect
 var start_template = load("res://start.tscn")
 var num_inputs = 2
 
-func _on_add_pressed():
-	num_inputs += 1
-	
-	var new_start = start_template.instance()
-	self.add_child(new_start)
-	
+func reposition_buttons():
 	var spacing = self.rect_size.y / num_inputs
 	var y_input = spacing
 	
@@ -16,6 +11,19 @@ func _on_add_pressed():
 		if "start" in i.get_name():
 			i.set_position(Vector2(9.728, y_input))
 			y_input += spacing
-			
+
+func _on_add_pressed():
+	num_inputs += 1
+	
+	var new_start = start_template.instance()
+	self.add_child(new_start)
+	reposition_buttons()
 	
 	print(new_start.get_name())
+
+
+func _on_remove_pressed():
+	if num_inputs > 1:
+		self.remove_child(self.get_child(num_inputs))
+		num_inputs -= 1
+		reposition_buttons()
