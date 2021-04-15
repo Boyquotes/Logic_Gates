c875ccf6a512419ba784ea5b42d261120ac2777f
Gr33nSkull <domen.sedlar123@gmail.com>
2021-04-15 16:08:17 +0200

    added output plug adding

---
+++ board.gd
@@ -0,0 +1,23 @@
+extends ColorRect
+
+var num_outputs = 5 
+var output_template = load("res://plug.tscn")
+
+func reposition_buttons():
+	var spacing = self.get_child(1).rect_size.y / (num_outputs - 4)
+	var y_input = spacing
+	
+	for i in self.get_children():
+		print(i.get_name())
+		if "@" in i.get_name():
+			i.set_position(Vector2(975, y_input))
+			y_input += spacing
+
+func _on_add_output_pressed():
+	num_outputs += 1
+	
+	var new_output = output_template.instance()
+	self.add_child(new_output)
+	reposition_buttons()
+	
+	print(new_output.get_name())
