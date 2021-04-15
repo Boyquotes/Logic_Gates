3137c19624674e77caddf04a356b2fc72eea775e
Gr33nSkull <domen.sedlar123@gmail.com>
2021-04-15 14:02:36 +0200

    popravu linkanje pluga

--- plug.gd
+++ plug.gd
@@ -10,6 +10,9 @@ var selected = false
 var button_area = Rect2(self.rect_position, self.rect_size)
 var lead = null
 
+# gleda ce je button pressed
+var p = false
+
 func _process(delta):
 	
 	#verafaying synchronazation with a link node and correcting it
@@ -24,14 +27,27 @@ func _process(delta):
 func _input(event):
 	#poveze se z gumbom s katerim ga povezes
 	if event is InputEventMouseButton:
-		if button_area.has_point(self.get_local_mouse_position()):
+		if button_area.has_point(self.get_local_mouse_position()) and p:
 			var board = self.get_parent()
+			var c = true
+			
 			for i in board.get_child(0).get_children():
 				for ii in i.get_children():
 					if "plug" in ii.get_name():
 						if ii.selected:
 							print(ii.on)
 							lead = ii
+							c = false
+			if c:
+				for i in board.get_children():
+					if "plug" in i.get_name():
+						print("i m", self.get_name())
+						print(i.get_name())
+		
+		if p:
+			p = false
+		else:
+			p = true
 
 func _on_plug_button_down():
 	pos = get_viewport().get_mouse_position()
